12c131e9f25d3824cf8476211e22c9326be2fca3
Natan Górecki <97natan@gmail.com>
2022-09-26 00:04:27 +0200

    added symbol known values

--- projects/06/src/SymbolTable.cpp
+++ projects/06/src/SymbolTable.cpp
@@ -27,7 +27,7 @@ int SymbolTable::getAddress(std::string symbol)
 	{
 		return it->second;
 	}
-	return 0;
+	return -1;
 }
 
 /// <summary>
@@ -35,5 +35,29 @@ int SymbolTable::getAddress(std::string symbol)
 /// </summary>
 SymbolTable::SymbolTable()
 {
+	addEntry(std::string("R0"),   0);
+	addEntry(std::string("R1"),   1);
+	addEntry(std::string("R2"),   2);
+	addEntry(std::string("R3"),   3);
+	addEntry(std::string("R4"),   4);
+	addEntry(std::string("R5"),   5);
+	addEntry(std::string("R6"),   6);
+	addEntry(std::string("R7"),   7);
+	addEntry(std::string("R8"),   8);
+	addEntry(std::string("R9"),   9);
+	addEntry(std::string("R10"), 10);
+	addEntry(std::string("R11"), 11);
+	addEntry(std::string("R12"), 12);
+	addEntry(std::string("R13"), 13);
+	addEntry(std::string("R14"), 14);
+	addEntry(std::string("R15"), 15);
 
+	addEntry(std::string("SP"),   0);
+	addEntry(std::string("LCL"),  1);
+	addEntry(std::string("ARG"),  2);
+	addEntry(std::string("THIS"), 3);
+	addEntry(std::string("THAT"), 4);
+
+	addEntry(std::string("SCREEN"), 16384);
+	addEntry(std::string("KBD"),    24576);
 }
