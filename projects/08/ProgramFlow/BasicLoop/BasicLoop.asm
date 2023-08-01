fe36ccabc06438a0c180dfae9cd4e49b5420bc0e
Natan Górecki <97natan@gmail.com>
2023-08-01 21:43:40 +0200

    generated ProgramFlow assembly code

---
+++ projects/08/ProgramFlow/BasicLoop/BasicLoop.asm
@@ -0,0 +1,153 @@
+(START)
+@256
+D=A
+@SP
+M=D
+
+// push constant 0
+@0
+D=A
+@SP
+A=M
+M=D
+@SP
+M=M+1
+
+// pop local 0
+@0
+D=A
+@LCL
+D=M+D
+@R13
+M=D
+@SP
+AM=M-1
+D=M
+@R13
+A=M
+M=D
+
+// label LOOP_START
+(Main$LOOP_START)
+
+// push argument 0
+@0
+D=A
+@ARG
+A=M+D
+D=M
+@SP
+A=M
+M=D
+@SP
+M=M+1
+
+// push local 0
+@0
+D=A
+@LCL
+A=M+D
+D=M
+@SP
+A=M
+M=D
+@SP
+M=M+1
+
+// add
+@SP
+AM=M-1
+D=M
+A=A-1
+M=M+D
+
+// pop local 0
+@0
+D=A
+@LCL
+D=M+D
+@R13
+M=D
+@SP
+AM=M-1
+D=M
+@R13
+A=M
+M=D
+
+// push argument 0
+@0
+D=A
+@ARG
+A=M+D
+D=M
+@SP
+A=M
+M=D
+@SP
+M=M+1
+
+// push constant 1
+@1
+D=A
+@SP
+A=M
+M=D
+@SP
+M=M+1
+
+// sub
+@SP
+AM=M-1
+D=M
+A=A-1
+M=M-D
+
+// pop argument 0
+@0
+D=A
+@ARG
+D=M+D
+@R13
+M=D
+@SP
+AM=M-1
+D=M
+@R13
+A=M
+M=D
+
+// push argument 0
+@0
+D=A
+@ARG
+A=M+D
+D=M
+@SP
+A=M
+M=D
+@SP
+M=M+1
+
+// if-goto LOOP_START
+@SP
+AM=M-1
+D=M
+@Main$LOOP_START
+D;JNE
+
+// push local 0
+@0
+D=A
+@LCL
+A=M+D
+D=M
+@SP
+A=M
+M=D
+@SP
+M=M+1
+
+(END)
+@END
+0;JMP
