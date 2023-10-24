da04491d3f5e2c5651cc8b91908ae99ab67274ec
Natan Górecki <97natan@gmail.com>
2023-10-25 01:22:40 +0200

    compilation enginge cleanup

--- projects/10/src/CompilationEngine.cpp
+++ projects/10/src/CompilationEngine.cpp
@@ -9,7 +9,6 @@ namespace fs = std::filesystem;
 
 std::function<void(std::string)> CompilationEngine::onWriteOutput;
 std::function<void(std::string)> CompilationEngine::onWriteToken;
-std::function<void()> CompilationEngine::onAdvanceToken;
 
 /// <summary>
 /// Opens the output file and gets ready to write into it.
@@ -44,15 +43,6 @@ CompilationEngine::CompilationEngine(std::string filename, JackTokenizer* jackTo
     {
         *mTokensFile << text;
     };
-    onAdvanceToken = [this]()
-    {
-        /*if (!mJackTokenizer->hasMoreTokens())
-        {
-            throw std::runtime_error("Failed to read token.");
-        }*/
-
-        mJackTokenizer->advance();
-    };
 }
 
 /// <summary>
@@ -84,203 +74,12 @@ CompilationEngine::~CompilationEngine()
 void CompilationEngine::compileClass()
 {
     auto classRule = new ClassRule();
-    classRule->initialize(mJackTokenizer);
+    auto result = classRule->initialize(mJackTokenizer);
+    if (!result)
+    {
+        throw std::runtime_error("Failed to initialize class.");
+    }
     classRule->compile();
-}
-
-/// <summary>
-/// Compiles a static variable declaration or a field declaration.
-/// </summary>
-void CompilationEngine::compileClassVarDec()
-{
-    /**mOutputFile << "<classVarDec>";
-
-    if (!Assert::That(ETokenType::KEYWORD, "static", "field"))
-    {
-        throw new std::runtime_error("compileClassVarDec - Missing keyword 'static' or 'field'.");
-    }
-    writeTokenToStreams();
-
-    advanceToken();
-    if (!Assert::That(ETokenType::KEYWORD, "int", "char", "boolean") && !Assert::That(ETokenType::IDENTIFIER))
-    {
-        throw std::runtime_error("compileType - Missing valid type: 'int', 'char', 'boolean' or class name.");
-    }
-    writeTokenToStreams();
-
-    do
-    {
-        advanceToken();
-        if (!Assert::That(ETokenType::IDENTIFIER))
-        {
-            throw std::runtime_error("compileClassVarDec - Missing variable name.");
-        }
-        writeTokenToStreams();
-
-        advanceToken();
-        if (!Assert::That(ETokenType::SYMBOL, ','))
-        {
-            break;
-        }
-        writeTokenToStreams();
-    } while (true);
-
-    if (!Assert::That(ETokenType::SYMBOL, ';'))
-    {
-        throw std::runtime_error("compileClassVarDec - Missing semi-colon ';' at the end of variable declaration.");
-    }
-
-    *mOutputFile << "</classVarDec>";*/
-}
-
-/// <summary>
-/// Compiles a complete method, function or constructor.
-/// </summary>
-void CompilationEngine::compileSubroutine()
-{
-    /**mOutputFile << "<subroutineDec>";
-
-    if (!Assert::That(ETokenType::KEYWORD, "constructor", "function", "method"))
-    {
-        throw std::runtime_error("compileSubroutine - Missing keyword 'constructor' or 'function' or 'method'.");
-    }    
-    writeTokenToStreams();
-
-
-
-    advanceToken();
-    if (!Assert::That(ETokenType::KEYWORD, "void") && !Assert::That(ETokenType::IDENTIFIER))
-    {
-        throw std::runtime_error("compileSubroutine - Missing return type.");
-    }
-    writeTokenToStreams();
-
-    advanceToken();
-    if (!Assert::That(ETokenType::IDENTIFIER))
-    {
-        throw std::runtime_error("compileSubroutine - Missing subroutine name.");
-    }
-    writeTokenToStreams();
-
-    advanceToken();
-    if (!Assert::That(ETokenType::SYMBOL, '('))
-    {
-        throw std::runtime_error("compileSubroutine - Missing left bracket '('.");
-    }
-    writeTokenToStreams();
-
-    compileParameterList();
-
-    advanceToken();
-    if (!Assert::That(ETokenType::SYMBOL, ')'))
-    {
-        throw std::runtime_error("compileSubroutine - Missing right bracket ')'.");
-    }
-    writeTokenToStreams();
-
-    compileSubroutineBody();
-
-    *mOutputFile << "</subroutineDec>";*/
-}
-
-/// <summary>
-/// Compiles a (possibly empty) parameter list. Does not handle the enclosing parentheses tokens ( and ).
-/// </summary>
-void CompilationEngine::compileParameterList()
-{
-
-}
-
-/// <summary>
-/// Compiles a subroutine's body.
-/// </summary>
-void CompilationEngine::compileSubroutineBody()
-{
-
-}
-
-/// <summary>
-/// Compiles a var declaration.
-/// </summary>
-void CompilationEngine::compileVarDec()
-{
-
-}
-
-/// <summary>
-/// Compiles a sequence of statements. Does not handle the enclosing curly bracket tokens { and }.
-/// </summary>
-void CompilationEngine::compileStatements()
-{
-
-}
-
-/// <summary>
-/// Compiles a let statement.
-/// </summary>
-void CompilationEngine::compileLet()
-{
-
-}
-
-/// <summary>
-/// Compiles an if statement, possibly with a trailing else clause.
-/// </summary>
-void CompilationEngine::compileIf()
-{
-
-}
-
-/// <summary>
-/// Compiles a while statement.
-/// </summary>
-void CompilationEngine::compileWhile()
-{
-
-}
-
-/// <summary>
-/// Compiles a do statement.
-/// </summary>
-void CompilationEngine::compileDo()
-{
-
-}
-
-/// <summary>
-/// Compiles a return statement.
-/// </summary>
-void CompilationEngine::compileReturn()
-{
-
-}
-
-/// <summary>
-/// Compiles an expression.
-/// </summary>
-void CompilationEngine::compileExpression()
-{
-
-}
-
-/// <summary>
-/// Compiles a term. If the current token is an identifier, the routine must resolve it
-///     into a variable, an array element or a subroutine call.
-/// A single lookahead token, which may be [, ( or . suffices to distinguish between the possibilities.
-/// Any other token is not part of this term and should not be advanced over.
-/// </summary>
-void CompilationEngine::compileTerm()
-{
-
-}
-
-/// <summary>
-/// Compiles a (possibly empty) comma-separated list of expressions.
-/// </summary>
-/// <returns>Returns the number of expressions in the list</returns>
-int CompilationEngine::compileExpressionList()
-{
-    return 0;
 }
 
 void CompilationEngine::writeOutput(std::string text)
@@ -291,9 +90,4 @@ void CompilationEngine::writeOutput(std::string text)
 void CompilationEngine::writeToken(std::string text)
 {
     onWriteToken(text);
-}
-
-void CompilationEngine::advanceToken()
-{
-    onAdvanceToken();
 }
--- projects/10/src/CompilationEngine.h
+++ projects/10/src/CompilationEngine.h
@@ -30,75 +30,13 @@ public:
     /// Compiles a complete class.
     /// </summary>
     void compileClass();
-    /// <summary>
-    /// Compiles a static variable declaration or a field declaration.
-    /// </summary>
-    void compileClassVarDec();
-    /// <summary>
-    /// Compiles a complete method, function or constructor.
-    /// </summary>
-    void compileSubroutine();
-    /// <summary>
-    /// Compiles a (possibly empty) parameter list. Does not handle the enclosing parentheses tokens ( and ).
-    /// </summary>
-    void compileParameterList();
-    /// <summary>
-    /// Compiles a subroutine's body.
-    /// </summary>
-    void compileSubroutineBody();
-    /// <summary>
-    /// Compiles a var declaration.
-    /// </summary>
-    void compileVarDec();
-    /// <summary>
-    /// Compiles a sequence of statements. Does not handle the enclosing curly bracket tokens { and }.
-    /// </summary>
-    void compileStatements();
-    /// <summary>
-    /// Compiles a let statement.
-    /// </summary>
-    void compileLet();
-    /// <summary>
-    /// Compiles an if statement, possibly with a trailing else clause.
-    /// </summary>
-    void compileIf();
-    /// <summary>
-    /// Compiles a while statement.
-    /// </summary>
-    void compileWhile();
-    /// <summary>
-    /// Compiles a do statement.
-    /// </summary>
-    void compileDo();
-    /// <summary>
-    /// Compiles a return statement.
-    /// </summary>
-    void compileReturn();
-    /// <summary>
-    /// Compiles an expression.
-    /// </summary>
-    void compileExpression();
-    /// <summary>
-    /// Compiles a term. If the current token is an identifier, the routine must resolve it
-    ///     into a variable, an array element or a subroutine call.
-    /// A single lookahead token, which may be [, ( or . suffices to distinguish between the possibilities.
-    /// Any other token is not part of this term and should not be advanced over.
-    /// </summary>
-    void compileTerm();
-    /// <summary>
-    /// Compiles a (possibly empty) comma-separated list of expressions.
-    /// </summary>
-    /// <returns>Returns the number of expressions in the list</returns>
-    int compileExpressionList();
 
 public:
     static void writeOutput(std::string text);
     static void writeToken(std::string text);
-    static void advanceToken();
 private:
     static std::function<void(std::string)> onWriteOutput;
     static std::function<void(std::string)> onWriteToken;
-    static std::function<void()> onAdvanceToken;
 
 private:
     std::ofstream* mOutputFile = NULL;
