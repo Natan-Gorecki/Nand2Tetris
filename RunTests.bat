153a51bc89cbe98fdd588277f8a369206b00cf5e
Natan Górecki <97natan@gmail.com>
2023-10-29 00:18:33 +0200

    review issues

--- RunTests.bat
+++ RunTests.bat
@@ -1,17 +1,17 @@
 @echo off
 
 if "%~1" neq "" (
-	set RepositoryDir=%~1
+    set RepositoryDir=%~1
 ) else (
     set RepositoryDir=C:\Projects\Nand2Tetris
 )
 
-echo "RepositoryDir: " %RepositoryDir%
+echo RepositoryDir: %RepositoryDir%
 
 
-echo "Tests for project 10: "
+echo Tests for project 10:
 call %RepositoryDir%\projects\10\RunTests.bat || goto :error
-echo "Tests for project 10 passed"
+echo Tests for project 10 passed
 
 
 :error
