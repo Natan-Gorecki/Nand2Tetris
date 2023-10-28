a1ae16454004935879a91188181a36b1bc61d281
Natan Górecki <97natan@gmail.com>
2023-10-28 21:41:38 +0200

    test separate shell

--- RunTests.bat
+++ RunTests.bat
@@ -1,6 +1,7 @@
-echo "yes"
+echo "RepositoryDir check"
 if not defined RepositoryDir (
 	set RepositoryDir=C:\Projects\Nand2Tetris
 )
 
+echo "Running %RepositoryDir%\projects\10\RunTests.bat"
 %RepositoryDir%\projects\10\RunTests.bat
