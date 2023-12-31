3a39eb41f46b14abfaa7ec22b6672dd33ea456e4
Natan Górecki <97natan@gmail.com>
2023-12-31 09:53:32 +0100

    refactored VMTranslator

--- src/VMTranslator/VMTranslator.cpp
+++ src/VMTranslator/VMTranslator.cpp
@@ -1,68 +1,61 @@
-#include "VMTranslator.h"
-
 #include <iostream>
 #include <filesystem>
 #include "Parser.h"
+#include "VMTranslator.h"
+#include "VMTranslatorError.h"
 
-#define SAFE_DELETE(x) if(x) { delete x; x = NULL; }
-
+using namespace std;
 namespace fs = std::filesystem;
 
-VMTranslator::VMTranslator(std::string path)
+VMTranslator::VMTranslator(const string& path)
 {
     fs::path filePath = path;
     fs::path inputPath = filePath.is_relative()
         ? fs::current_path().string() + "\\" + filePath.string() 
         : filePath.string();
     
-    this->input_file = inputPath.string();
-    this->is_directory_path = inputPath.extension() == "";
+    mInputFileName = inputPath.string();
+    mIsDirectoryPath = inputPath.extension() == "";
 
-    std::string outputPrefix = filePath.is_relative()
+    string outputPrefix = filePath.is_relative()
         ? fs::current_path().string() + "\\"
         : "";
-    std::string outputPath = this->is_directory_path
+    string outputPath = mIsDirectoryPath
         ? filePath.string()
         : filePath.parent_path().string();
-    std::string outputFileName = this->is_directory_path
+    string outputFileName = mIsDirectoryPath
         ? filePath.filename().string()
         : filePath.filename().stem().string();
 
-    this->output_file= outputPrefix + outputPath + "\\" + outputFileName + ".asm";
+    mOutputFileName = outputPrefix + outputPath + "\\" + outputFileName + ".asm";
 
-    if (!is_directory_path && !isupper(inputPath.filename().string()[0]))
+    if (!mIsDirectoryPath && !isupper(inputPath.filename().string()[0]))
     {
-        throw std::runtime_error("Input file/directory " + std::string(path) + " doesn't start with uppercase.");
+        throw VMTranslatorError("Input file/directory " + string(path) + " doesn't start with uppercase.");
     }
 
-    if (!is_directory_path && inputPath.extension() != ".vm")
+    if (!mIsDirectoryPath && inputPath.extension() != ".vm")
     {
-        throw std::runtime_error("Input file " + std::string(path) + " doesn't have .vm extension");
+        throw VMTranslatorError("Input file " + string(path) + " doesn't have .vm extension");
     }
 
-    code_writer = new CodeWriter(output_file, true);
+    mCodeWriter = make_unique<CodeWriter>(mOutputFileName, true);
 }
 
-VMTranslator::~VMTranslator()
+bool VMTranslator::isDirectoryPath() const
 {
-    SAFE_DELETE(code_writer);
-}
-
-bool VMTranslator::isDirectoryPath()
-{
-    return is_directory_path;
+    return mIsDirectoryPath;
 }
 
 void VMTranslator::parseDirectory()
 {
-    fs::path filePath = fs::path(input_file);
-    if (!fs::exists(filePath))
+    if (auto filePath = fs::path(mInputFileName); !fs::exists(filePath))
     {
-        throw std::runtime_error("Directory " + input_file + " doesn't exist");
+        throw VMTranslatorError("Directory " + mInputFileName + " doesn't exist");
     }
 
-    std::vector<std::string> vmFiles = std::vector<std::string>();
-    for (const fs::directory_entry& entry : fs::directory_iterator(input_file))
+    auto vmFiles = vector<string>();
+    for (const auto& entry : fs::directory_iterator(mInputFileName))
     {
         if (entry.is_regular_file() && isupper(entry.path().filename().string()[0]) && entry.path().extension() == ".vm")
         {
@@ -70,50 +63,49 @@ void VMTranslator::parseDirectory()
         }
     }
     
-    if (vmFiles.size() == 0)
+    if (vmFiles.empty())
     {
-        throw std::runtime_error("Directory " + input_file + " doesn't contain any .vm extension");
+        throw VMTranslatorError("Directory " + mInputFileName + " doesn't contain any .vm extension");
     }
 
-    for (std::string vmFile : vmFiles)
+    for (auto& vmFile : vmFiles)
     {
         parseSingleFile(vmFile);
     }
 
-    code_writer->validateGotoStatements();
+    mCodeWriter->validateGotoStatements();
 }
 
 void VMTranslator::parseSingleFile()
 {
-    fs::path filePath = fs::path(input_file);
-    if (!fs::exists(filePath))
+    if (auto filePath = fs::path(mInputFileName); !fs::exists(filePath))
     {
-        throw std::runtime_error("File " + input_file + " doesn't exist");
+        throw VMTranslatorError("File " + mInputFileName + " doesn't exist");
     }
-    
-    parseSingleFile(input_file);
 
-    code_writer->validateGotoStatements();
+    parseSingleFile(mInputFileName);
+
+    mCodeWriter->validateGotoStatements();
 }
 
-std::string VMTranslator::getOutputFile()
+string VMTranslator::getOutputFile() const
 {
-    return this->output_file;
+    return mOutputFileName;
 }
 
-void VMTranslator::parseSingleFile(std::string path)
+void VMTranslator::parseSingleFile(const string& path)
 {
-    Parser parser = Parser(path);
+    auto parser = Parser(path);
 
-    std::string fileName = fs::path(path).replace_extension("").filename().string();
-    code_writer->setFileName(fileName);
+    auto fileName = fs::path(path).replace_extension("").filename().string();
+    mCodeWriter->setFileName(fileName);
 
     while (parser.hasMoreLines())
     {
         parser.advance();
         if (parser.commandType() == ECommandType::UNDEFINED)
         {
-            throw std::runtime_error("Unknown command.");
+            throw VMTranslatorError("Unknown command.");
         }
         else if (parser.commandType() == ECommandType::COMMENT)
         {
@@ -121,35 +113,35 @@ void VMTranslator::parseSingleFile(std::string path)
         }
         else if (parser.commandType() == ECommandType::C_ARITHMETIC)
         {
-            code_writer->writeArithmetic(parser.arg1());
+            mCodeWriter->writeArithmetic(parser.arg1());
         }
         else if (parser.commandType() == ECommandType::C_PUSH || parser.commandType() == ECommandType::C_POP)
         {
-            code_writer->writePushPop(parser.commandType(), parser.arg1(), parser.arg2());
+            mCodeWriter->writePushPop(parser.commandType(), parser.arg1(), parser.arg2());
         }
         else if (parser.commandType() == ECommandType::C_LABEL)
         {
-            code_writer->writeLabel(parser.arg1());
+            mCodeWriter->writeLabel(parser.arg1());
         }
         else if (parser.commandType() == ECommandType::C_GOTO)
         {
-            code_writer->writeGoto(parser.arg1());
+            mCodeWriter->writeGoto(parser.arg1());
         }
         else if (parser.commandType() == ECommandType::C_IF)
         {
-            code_writer->writeIf(parser.arg1());
+            mCodeWriter->writeIf(parser.arg1());
         }
         else if (parser.commandType() == ECommandType::C_FUNCTION)
         {
-            code_writer->writeFunction(parser.arg1(), parser.arg2());
+            mCodeWriter->writeFunction(parser.arg1(), parser.arg2());
         }
         else if (parser.commandType() == ECommandType::C_RETURN)
         {
-            code_writer->writeReturn();
+            mCodeWriter->writeReturn();
         }
         else if (parser.commandType() == ECommandType::C_CALL)
         {
-            code_writer->writeCall(parser.arg1(), parser.arg2());
+            mCodeWriter->writeCall(parser.arg1(), parser.arg2());
         }
     }
 }
--- src/VMTranslator/VMTranslator.h
+++ src/VMTranslator/VMTranslator.h
@@ -4,48 +4,42 @@
 
 /// <summary>
 /// VMTranslator contains main implementation to parse single file or all files from the specified directory. 
-///		It converts code from .vm files and creates single .asm file.
+///     It converts code from .vm files and creates single .asm file.
 /// </summary>
 class VMTranslator
 {
 public:
-	/// <summary>
-	/// Valides provided path, creates CodeWriter. 
-	/// May throw a runtime exception.
-	/// </summary>
-	/// <param name="path"></param>
-	VMTranslator(std::string path);
-	/// <summary>
-	/// Releases alocated memory.
-	/// </summary>
-	~VMTranslator();
+    /// <summary>
+    /// Valides provided path, creates CodeWriter. 
+    /// May throw a runtime exception.
+    /// </summary>
+    /// <param name="path"></param>
+    VMTranslator(const std::string& path);
 
-public:
-	/// <summary>
-	/// Returns an information, whether provided path is directory or single file.
-	/// </summary>
-	bool isDirectoryPath();
-	/// <summary>
-	/// Parses all .vm files from specified directory.
-	/// </summary>
-	void parseDirectory();
-	/// <summary>
-	/// Parses specified .vm file.
-	/// </summary>
-	void parseSingleFile();
-	/// <summary>
-	/// Gets output file name.
-	/// </summary>
-	/// <returns></returns>
-	std::string getOutputFile();
+    /// <summary>
+    /// Returns an information, whether provided path is directory or single file.
+    /// </summary>
+    bool isDirectoryPath() const;
+    /// <summary>
+    /// Parses all .vm files from specified directory.
+    /// </summary>
+    void parseDirectory();
+    /// <summary>
+    /// Parses specified .vm file.
+    /// </summary>
+    void parseSingleFile();
+    /// <summary>
+    /// Gets output file name.
+    /// </summary>
+    /// <returns></returns>
+    std::string getOutputFile() const;
 
 private:
-	void parseSingleFile(std::string path);
+    void parseSingleFile(const std::string& path);
 
-private:
-	std::string input_file;
-	std::string output_file;
-	bool is_directory_path;
+    std::string mInputFileName;
+    std::string mOutputFileName;
+    bool mIsDirectoryPath;
 
-	CodeWriter* code_writer = NULL;
+    std::unique_ptr<CodeWriter> mCodeWriter;
 };
