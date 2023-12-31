93e99bd0ec395a66d977cf735ff5f00665f91547
Natan Górecki <97natan@gmail.com>
2023-12-31 09:57:07 +0100

    replaced tabs with spaces

--- src/VMTranslator/CodeWriter.h
+++ src/VMTranslator/CodeWriter.h
@@ -11,101 +11,101 @@
 class CodeWriter
 {
 public:
-	/// <summary>
-	/// Opens the output file and gets ready to write into it
-	/// </summary>
-	/// <param name="filename">Name of the output file</param>
-	CodeWriter(std::string filename, bool generateComment);
-	/// <summary>
-	/// Closes the output file
-	///</summary>
-	~CodeWriter();
+    /// <summary>
+    /// Opens the output file and gets ready to write into it
+    /// </summary>
+    /// <param name="filename">Name of the output file</param>
+    CodeWriter(std::string filename, bool generateComment);
+    /// <summary>
+    /// Closes the output file
+    ///</summary>
+    ~CodeWriter();
 
 public:
-	/// <summary>
-	/// Informs that the translation of a new VM file has started (called by VMTranslator).
-	/// </summary>
-	/// <param name="fileName">File name</param>
-	void setFileName(std::string fileName);
-	/// <summary>
-	/// Writes to the output file the assembly code that implements the given arithmetic-logical command.
-	/// </summary>
-	/// <param name="command">Arithmetic-logical command</param>
-	void writeArithmetic(std::string command);
-	/// <summary>
-	/// Writes to the output file the assembly code that implements the given push or pop command
-	/// </summary>
-	/// <param name="commandType">C_PUSH or C_POP</param>
-	/// <param name="segment">Name of the segment</param>
-	/// <param name="index">Numeric address</param>
-	void writePushPop(ECommandType commandType, std::string segment, int index);
-	/// <summary>
-	/// Writes assembly code that effects the label command
-	/// </summary>
-	/// <param name="label">Label name</param>
-	void writeLabel(std::string label);
-	/// <summary>
-	/// Writes assembly code that effects the goto command.
-	/// </summary>
-	/// <param name="label">Label name</param>
-	void writeGoto(std::string label);
-	/// <summary>
-	/// Writes assembly code that effects the if-goto command.
-	/// </summary>
-	/// <param name="label">Label name</param>
-	void writeIf(std::string label);
-	/// <summary>
-	/// Writes assembly code that effects the function command.
-	/// </summary>
-	/// <param name="functionName">Function name</param>
-	/// <param name="nVars">Count of the function local variables</param>
-	void writeFunction(std::string functionName, int nVars);
-	/// <summary>
-	/// Writes assembly code that effects the call command.
-	/// </summary>
-	/// <param name="functionName">Function name</param>
-	/// <param name="nArgs">Count of the arguments that have been pushed onto the stack before the call</param>
-	void writeCall(std::string functionName, int nArgs);
-	/// <summary>
-	/// Writes assembly code that effects the return command.
-	/// </summary>
-	void writeReturn();
-	/// <summary>
-	/// Validates if every label used in goto and if-goto is defined.
-	/// </summary>
-	void validateGotoStatements();
+    /// <summary>
+    /// Informs that the translation of a new VM file has started (called by VMTranslator).
+    /// </summary>
+    /// <param name="fileName">File name</param>
+    void setFileName(std::string fileName);
+    /// <summary>
+    /// Writes to the output file the assembly code that implements the given arithmetic-logical command.
+    /// </summary>
+    /// <param name="command">Arithmetic-logical command</param>
+    void writeArithmetic(std::string command);
+    /// <summary>
+    /// Writes to the output file the assembly code that implements the given push or pop command
+    /// </summary>
+    /// <param name="commandType">C_PUSH or C_POP</param>
+    /// <param name="segment">Name of the segment</param>
+    /// <param name="index">Numeric address</param>
+    void writePushPop(ECommandType commandType, std::string segment, int index);
+    /// <summary>
+    /// Writes assembly code that effects the label command
+    /// </summary>
+    /// <param name="label">Label name</param>
+    void writeLabel(std::string label);
+    /// <summary>
+    /// Writes assembly code that effects the goto command.
+    /// </summary>
+    /// <param name="label">Label name</param>
+    void writeGoto(std::string label);
+    /// <summary>
+    /// Writes assembly code that effects the if-goto command.
+    /// </summary>
+    /// <param name="label">Label name</param>
+    void writeIf(std::string label);
+    /// <summary>
+    /// Writes assembly code that effects the function command.
+    /// </summary>
+    /// <param name="functionName">Function name</param>
+    /// <param name="nVars">Count of the function local variables</param>
+    void writeFunction(std::string functionName, int nVars);
+    /// <summary>
+    /// Writes assembly code that effects the call command.
+    /// </summary>
+    /// <param name="functionName">Function name</param>
+    /// <param name="nArgs">Count of the arguments that have been pushed onto the stack before the call</param>
+    void writeCall(std::string functionName, int nArgs);
+    /// <summary>
+    /// Writes assembly code that effects the return command.
+    /// </summary>
+    void writeReturn();
+    /// <summary>
+    /// Validates if every label used in goto and if-goto is defined.
+    /// </summary>
+    void validateGotoStatements();
 
 private:
-	void initialCode();
-	void finalCode();
+    void initialCode();
+    void finalCode();
 
-	void writeComparisonCommand(std::string comparisonCheck);
+    void writeComparisonCommand(std::string comparisonCheck);
 
-	void writeLocalArgThisThat(ECommandType commandType, std::string segmentName, int index);
-	void writePointer(ECommandType commandType, int index);
-	void writeTemp(ECommandType commandType, int index);
-	void writeConstant(ECommandType commandType, int index);
-	void writeStatic(ECommandType commandType, int index);
+    void writeLocalArgThisThat(ECommandType commandType, std::string segmentName, int index);
+    void writePointer(ECommandType commandType, int index);
+    void writeTemp(ECommandType commandType, int index);
+    void writeConstant(ECommandType commandType, int index);
+    void writeStatic(ECommandType commandType, int index);
 
-	void setFunctionName(std::string functionName);
-	std::string getFullLabelName(std::string label);
+    void setFunctionName(std::string functionName);
+    std::string getFullLabelName(std::string label);
 
-	std::string stack2DRegister();
-	std::string DRegister2Stack();
-	int getNumber(std::string label);
+    std::string stack2DRegister();
+    std::string DRegister2Stack();
+    int getNumber(std::string label);
 
 private:
-	std::ofstream* output_file = NULL;
-	bool generate_comment = false;
+    std::ofstream* output_file = NULL;
+    bool generate_comment = false;
 
-	std::unordered_map<std::string, int> counters_map = std::unordered_map<std::string, int>();
-	bool is_comparison_used = false;
+    std::unordered_map<std::string, int> counters_map = std::unordered_map<std::string, int>();
+    bool is_comparison_used = false;
 
-	std::set<std::string> defined_labels = std::set<std::string>();
-	std::set<std::string> defined_goto = std::set<std::string>();
-	
-	std::string file_name = "";
-	std::string function_name = "";
-	std::string default_function_name = "Main";
-	bool no_function_defined = true;
+    std::set<std::string> defined_labels = std::set<std::string>();
+    std::set<std::string> defined_goto = std::set<std::string>();
+    
+    std::string file_name = "";
+    std::string function_name = "";
+    std::string default_function_name = "Main";
+    bool no_function_defined = true;
 };
--- src/VMTranslator/Parser.h
+++ src/VMTranslator/Parser.h
@@ -10,49 +10,49 @@
 /// </summary>
 class Parser {
 public:
-	/// <summary>
-	/// Opens the input file and gets ready to parse it.
-	/// </summary>
-	/// <param name="filename">Name of the input file</param>
-	Parser(std::string filename);
-	/// <summary>
-	/// Closes the input file and cleans up.
-	/// </summary>
-	~Parser();
+    /// <summary>
+    /// Opens the input file and gets ready to parse it.
+    /// </summary>
+    /// <param name="filename">Name of the input file</param>
+    Parser(std::string filename);
+    /// <summary>
+    /// Closes the input file and cleans up.
+    /// </summary>
+    ~Parser();
 
 public:
-	/// <summary>
-	/// Are there more lines in the input?
-	/// </summary>
-	bool hasMoreLines();
-	/// <summary>
-	/// Reads the next command from the input and makes it the current command.
-	/// <para/> The routine should be called only if hasMoreLines is true.
-	/// <para/> Initially there is no current command.
-	/// </summary>
-	void advance();
-	/// <summary>
-	/// Returns a constant representing the type of the current command.
-	/// <para/> If the current command is an arithmetic-logical command, returns C_ARITHMETIC.
-	/// </summary>
-	ECommandType commandType();
-	/// <summary>
-	/// Returns the first argument of the current command.
-	/// <para/> In the case of C_ARITHMETIC, the command itself (add, sub, etc.) is returned.
-	/// <para/> Should not be called if the current command is C_RETURN.
-	/// </summary>
-	std::string arg1();
-	/// <summary>
-	/// Returns the second argument of the current command.
-	/// <para/> Should be called only if the current command is C_PUSH, C_POP, C_FUNCTION, or C_CALL.
-	/// </summary>
-	int arg2();
+    /// <summary>
+    /// Are there more lines in the input?
+    /// </summary>
+    bool hasMoreLines();
+    /// <summary>
+    /// Reads the next command from the input and makes it the current command.
+    /// <para/> The routine should be called only if hasMoreLines is true.
+    /// <para/> Initially there is no current command.
+    /// </summary>
+    void advance();
+    /// <summary>
+    /// Returns a constant representing the type of the current command.
+    /// <para/> If the current command is an arithmetic-logical command, returns C_ARITHMETIC.
+    /// </summary>
+    ECommandType commandType();
+    /// <summary>
+    /// Returns the first argument of the current command.
+    /// <para/> In the case of C_ARITHMETIC, the command itself (add, sub, etc.) is returned.
+    /// <para/> Should not be called if the current command is C_RETURN.
+    /// </summary>
+    std::string arg1();
+    /// <summary>
+    /// Returns the second argument of the current command.
+    /// <para/> Should be called only if the current command is C_PUSH, C_POP, C_FUNCTION, or C_CALL.
+    /// </summary>
+    int arg2();
 
 private:
-	std::ifstream* input_stream = NULL;
-	std::string current_line = "";
+    std::ifstream* input_stream = NULL;
+    std::string current_line = "";
 
-	ECommandType f_command_type = ECommandType::UNDEFINED;
-	std::string f_arg1 = "";
-	int f_arg2 = -1;
+    ECommandType f_command_type = ECommandType::UNDEFINED;
+    std::string f_arg1 = "";
+    int f_arg2 = -1;
 };
