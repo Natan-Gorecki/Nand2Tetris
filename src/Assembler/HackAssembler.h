e8209d99a426377bf0c97cb64b82356bf36e4c10
Natan Górecki <97natan@gmail.com>
2023-12-31 07:27:36 +0100

    refactored HackAssembler

--- src/Assembler/HackAssembler.h
+++ src/Assembler/HackAssembler.h
@@ -1,4 +1,5 @@
 #pragma once
+
 #include <string>
 
 #include "CodeModule.h"
@@ -7,35 +8,37 @@
 
 /// <summary>
 /// HackAssembler contains main implementation to map symbols from input file,
-///		then to convert assembler code to machine instructions
+///     then to convert assembler code to machine instructions
 /// <summary>
 class HackAssembler
 {
 public:
-	/// <summary>
-	/// Maps symbols from input file (1st run)
-	/// </summary>
-	void searchSymbols();
-	/// <summary>
-	/// Converts assembler code to machine instructions (2nd run)
-	/// </summary>
-	void assemblerToMachineCode(bool allowOverflowError);
+    /// <summary>
+    /// Maps symbols from input file (1st run)
+    /// </summary>
+    void searchSymbols();
+    /// <summary>
+    /// Converts assembler code to machine instructions (2nd run)
+    /// </summary>
+    void assemblerToMachineCode(bool allowOverflowError);
 
 public:
-	/// <summary>
-	/// Creates parser, symbol table and handles file paths
-	/// </summary>
-	HackAssembler(std::string inputFile);
-	/// <summary>
-	/// Releases allocated memory
-	/// </summary>
-	~HackAssembler();
+    /// <summary>
+    /// Creates parser, symbol table and handles file paths
+    /// </summary>
+    HackAssembler(std::string inputFile);
+    /// <summary>
+    /// Releases allocated memory
+    /// </summary>
+    ~HackAssembler();
 
 private:
-	Parser* parser = NULL;
-	SymbolTable* symbol_table = NULL;
+    void writeAInstruction(std::ofstream* outputStream, int& variableAddress, bool allowOverflowError);
+    void writeCInstruction(std::ofstream* outputStream);
 
-	std::string input_file;
-	std::string output_file;
+    Parser* parser = NULL;
+    SymbolTable* symbol_table = NULL;
+
+    std::string input_file;
+    std::string output_file;
 };
-
